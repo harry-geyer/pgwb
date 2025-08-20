38fcdcf92567ea61eb75dda3481e4faa0440db7d
Harry Geyer <harrygeyer2@gmail.com>
2025-08-20 13:07:07 +0100

    Correct OpenGL version hint.

--- src/render.c
+++ src/render.c
@@ -86,8 +86,8 @@ void render(void)
         return;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
